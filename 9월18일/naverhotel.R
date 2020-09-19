a69b5f05b4e1aedcf97bb628eaa5ab2216c2398f
Minyoung Kim <sgm02101@hanmail.net>
2020-09-19 18:36:38 +0900

    update9_19

---
+++ 9월18일/naverhotel.R
@@ -0,0 +1,29 @@
+library(RSelenium)
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+remDr$open()
+url <- "https://hotel.naver.com/hotels/item?hotelId=hotel:Shilla_Stay_Yeoksam&destination_kor=%EC%8B%A0%EB%9D%BC%EC%8A%A4%ED%85%8C%EC%9D%B4%20%EC%97%AD%EC%82%BC&rooms=2"
+remDr$navigate(url)
+Sys.sleep(3)
+pageLink <- NULL
+reple <- 0
+curr_PageOldNum <- 0
+
+body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > span:nth-child(3) > strong
+repeat{
+  doms <- remDr$findElements(using = "css selector", ".review_desc > p")
+  Sys.sleep(1)
+  reple_v <- sapply(doms, function (x) {x$getElementText()})
+  print(reple_v)
+  un_v <- unlist(reple_v)
+  if(reple[length(reple)] == un_v[length(un_v)]){
+    break
+  }
+  reple <- append(reple, un_v)
+  cat(length(reple), "\n")
+
+  pageLink <- remDr$findElements(using='css','.review_ta.ng-scope > div.paginate > a.direction.next')
+  remDr$executeScript("arguments[0].click();",pageLink)
+}
+cat(length(reple[-1]), "개의 댓글 추출\n")
+write(reple[-1],"naverhotel.txt")
+
